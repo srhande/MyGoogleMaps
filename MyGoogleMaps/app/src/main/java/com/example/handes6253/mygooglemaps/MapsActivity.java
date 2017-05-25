17693e52cca150b43d4d470122f3495ba7d54d1c
srhande <srhande@gmail.com>
2017-05-25 11:09:38 -0700

    worked on current location marker + switching map view

--- MyGoogleMaps/app/src/main/java/com/example/handes6253/mygooglemaps/MapsActivity.java
+++ MyGoogleMaps/app/src/main/java/com/example/handes6253/mygooglemaps/MapsActivity.java
@@ -1,7 +1,10 @@
 package com.example.handes6253.mygooglemaps;
 
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,6 +17,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private boolean myLocationEnabled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,18 +44,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng mumbai = new LatLng(19.2326911,72.8583207);
+        LatLng mumbai = new LatLng(19.2326911, 72.8583207);
         mMap.addMarker(new MarkerOptions().position(mumbai).title("Marker in Mumbai"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(mumbai));
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+           return;
+        }
+        mMap.setMyLocationEnabled(true);
     }
 
-    public void onConnected(Bundle connectionHint){
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                mGoogleApiClient);
-        if (mLastLocation != null) {
-            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
-            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+    public void switchView(View v) {
+        if (mMap.getMapType() == GoogleMap.MAP_TYPE_NORMAL) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         }
-    }
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
 }
